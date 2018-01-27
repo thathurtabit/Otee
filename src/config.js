a7302ee0b927ba77081ce855a1e65e8aa342e85e
Stephen Fairbanks <biggste2000@hotmail.com>
2018-01-27 16:28:55 +0000

    Feat: Basic CSV tilemap, basic collision detection and movement

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  gameWidth: 800,
+  gameWidth: 410,
   gameHeight: 600,
   localStorageName: 'phaseres6webpack'
 }
--- src/states/Game.js
+++ src/states/Game.js
@@ -1,55 +1,46 @@
 /* globals __DEV__ */
 import Phaser from 'phaser'
 
-// Set init global vars
-// var player
-// var platforms
-
 export default class extends Phaser.State {
   constructor () {
     super()
     this.player = this.player
-    this.platforms = this.platforms
-    this.hitPlatform = this.hitPlatform
+    this.walls = this.walls
+    this.hitWall = this.hitWall
     this.cursors = this.cursors
     this.stars = this.stars
     this.score = 0
     this.scoreText = this.scoreText
+    this.tileMap = this.tileMap
+    this.mapLayer = this.mapLayer
+    this.direction = 'down'
   }
 
   init () {}
   preload () {
     this.load.image('sky', 'assets/images/sky.png')
-    this.load.image('ground', 'assets/images/platform.png')
+    this.load.tilemap('map', 'assets/data/phaser-game-tile.csv', null, Phaser.Tilemap.CSV)
+    this.load.image('blocks', 'assets/images/tiles.png')
     this.load.image('star', 'assets/images/star.png')
-    this.load.spritesheet('dude', 'assets/images/dude.png', 32, 48)
+    this.load.spritesheet('dude', 'assets/images/dude.png', 15, 15)
   }
 
-  // Platforms
-  platformBuilder () {
-    //  The platforms group contains the ground and the 2 ledges we can jump on
-    this.platforms = this.add.group()
+  // Walls
+  wallBuilder () {
+    //  Because we're loading CSV map data we have to specify the tile size here or we can't render it
+    this.tileMap = this.add.tilemap('map', 41, 41)
 
-    //  We will enable physics for any object that is created in this group
-    this.platforms.enableBody = true
+    //  Add a Tileset image to the map
+    this.tileMap.addTilesetImage('blocks')
 
-    // Here we create the ground.
-    let ground = this.platforms.create(0, this.world.height - 64, 'ground')
+    // Creates a map layer
+    this.mapLayer = this.tileMap.createLayer(0)
 
-    //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
-    ground.scale.setTo(2, 2)
+    //  Resize the world
+    this.mapLayer.resizeWorld()
 
-    //  This stops it from falling away when you jump on it
-    ground.body.immovable = true
-
-    //  Now let's create two ledges
-    let ledge = this.platforms.create(400, 400, 'ground')
-
-    ledge.body.immovable = true
-
-    ledge = this.platforms.create(-150, 250, 'ground')
-
-    ledge.body.immovable = true
+    // Collision
+    this.tileMap.setCollisionByExclusion([1])
   }
 
   // Handle overlap between player and object
@@ -88,19 +79,18 @@ export default class extends Phaser.State {
   // Player
   playerBuilder () {
     // The player and its settings
-    this.player = this.add.sprite(32, 0, 'dude')
+    this.player = this.add.sprite(80, 50, 'dude')
 
     //  We need to enable physics on the player
     this.physics.arcade.enable(this.player)
 
-    //  Player physics properties. Give the little guy a slight bounce.
-    this.player.body.bounce.y = 0.1
-    this.player.body.gravity.y = 400
     this.player.body.collideWorldBounds = true
 
     //  Our two animations, walking left and right.
     this.player.animations.add('left', [0, 1, 2, 3], 10, true)
     this.player.animations.add('right', [5, 6, 7, 8], 10, true)
+    this.player.animations.add('up', [0, 1, 2, 3], 10, true)
+    this.player.animations.add('down', [5, 6, 7, 8], 10, true)
   }
 
   // Movement events
@@ -108,25 +98,47 @@ export default class extends Phaser.State {
     this.cursors = this.input.keyboard.createCursorKeys()
     //  Reset the players velocity (movementEvents)
     this.player.body.velocity.x = 0
+    this.player.body.velocity.y = 0
 
     if (this.cursors.left.isDown) {
-      //  Move to the left
-      this.player.body.velocity.x = -150
-
-      this.player.animations.play('left')
+      //  Move left
+      this.direction = 'left'
     } else if (this.cursors.right.isDown) {
-      //  Move to the right
-      this.player.body.velocity.x = 150
+      //  Move right
+      this.direction = 'right'
+    } else if (this.cursors.up.isDown) {
+      //  Move up
+      this.direction = 'up'
+    } else if (this.cursors.down.isDown) {
+      //  Move down
+      this.direction = 'down'
+    }
+  }
+
+  // TODO: Replace with Switch
+  movePlayer () {
+    if (this.direction === 'left') {
+      this.player.body.velocity.x -= 100
+      this.player.animations.play('left')
+      console.log('moving left')
+    } else if (this.direction === 'right') {
+      this.player.body.velocity.x += 100
       this.player.animations.play('right')
-    } else {
-      //  Stand still
-      this.player.animations.stop()
-      this.player.frame = 4
+      console.log('moving right')
+    } else if (this.direction === 'up') {
+      this.player.body.velocity.y -= 100
+      this.player.animations.play('up')
+      console.log('moving up')
+    } else if (this.direction === 'down') {
+      this.player.body.velocity.y += 100
+      this.player.animations.play('down')
+      console.log('moving down')
     }
-    //  Allow the player to jump if they are touching the ground.
-    if (this.cursors.up.isDown && this.player.body.touching.down && this.hitPlatform) {
-      this.player.body.velocity.y = -350
-    }
+  }
+
+  handleGameOver () {
+    this.direction = null
+    this.scoreText.text = `GAME OVER`
   }
 
   // CREATE THE THINGS
@@ -134,25 +146,26 @@ export default class extends Phaser.State {
     //  We're going to be using physics, so enable the Arcade Physics system
     this.physics.startSystem(Phaser.Physics.ARCADE)
 
-    //  A simple background for our game
+    //  A simple backblock for our game
     this.add.sprite(0, 0, 'sky')
 
-    this.platformBuilder()
+    this.wallBuilder()
     this.playerBuilder()
     this.starGroup()
 
-    this.scoreText = this.add.text(16, 16, 'Score: 0', { fontSize: '18px', fill: '#000' })
+    this.scoreText = this.add.text(16, 16, 'Score: 0', { fontSize: '18px', fill: '#fff' })
   }
 
   update () {
-    // Collide the player and the stars with the platforms
-    this.hitPlatform = this.physics.arcade.collide(this.player, this.platforms)
-    // Stars colliding with platforms
-    this.physics.arcade.collide(this.stars, this.platforms)
+    // Collide the player and the stars with the walls
+    this.hitWall = this.physics.arcade.collide(this.player, this.mapLayer, this.handleGameOver, null, this)
+    // Stars colliding with walls
+    this.physics.arcade.collide(this.stars, this.mapLayer)
     // This for keyboard events
     this.physics.arcade.overlap(this.player, this.stars, this.collectStar, null, this)
     // This for keyboard events
     this.movementEvents()
+    this.movePlayer()
   }
 
   render () {
